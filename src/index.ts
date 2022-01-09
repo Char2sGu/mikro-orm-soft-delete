9a98b2e87fc5b5e335ff1c64e2dd9419b987e1df
TheNightmareX <TheNightmareX@foxmail.com>
2022-01-09 18:29:59 +0800

    feat: add index.ts

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export { SoftDeletable } from "./soft-deletable.decorator";
+export { SOFT_DELETABLE } from "./soft-deletable.symbol";
+export { SOFT_DELETABLE_FILTER } from "./soft-deletable-filter.constant";
+export { SoftDeletableFilterArgs } from "./soft-deletable-filter-args.interface";
+export { SoftDeletableHandlerSubscriber } from "./soft-deletable-handler.subscriber";
+export { SoftDeletableMetadata } from "./soft-deletable-metadata.interface";
